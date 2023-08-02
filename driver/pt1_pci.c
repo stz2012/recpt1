cf3e68db29942436ef8e1efbdad8be2fcf183a9a
stz2012 <tslroom@hotmail.com>
2023-08-02 15:07:13 +0900

    Support for Kernel 6.4

--- driver/pt1_pci.c
+++ driver/pt1_pci.c
@@ -1033,7 +1033,11 @@ static struct pci_driver pt1_driver = {
 static int __init pt1_pci_init(void)
 {
 	printk(KERN_INFO "%s", version);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,4,0)
+	pt1video_class = class_create(DRIVERNAME);
+#else
 	pt1video_class = class_create(THIS_MODULE, DRIVERNAME);
+#endif
 	if (IS_ERR(pt1video_class))
 		return PTR_ERR(pt1video_class);
 	return pci_register_driver(&pt1_driver);
